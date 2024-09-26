7296fa3ac01b40fc84bdbffb0135a7a5ba0ea445
Spancello <gab.gcampos07@gmail.com>
2024-09-26 20:27:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,322 @@
+//var nome = prompt("Qual o seu nome?")
+//alert("seja bem vindo " + nome )
+//var altura = prompt("coleque a altura ")
+//var peso = prompt("coleque o peso ")
+//alert(numero1 / numero2)
+
+//var numero1 = Number(prompt("qual o primeiro numero"))
+//var numero2 = Number(prompt("qual o segundo numero"))
+
+//var (" + peso / (altura * altura))
+//alert("o resultado da soma dos dois é: " + (numero1 - numero2))
+//alert("o dobro de " + numero1 + " é " + numero1 * 2 + "o triplo de " + numero1 + " é " + numero1 * 3)
+//alert("o resultado da soma dos dois é: " + (numero1 / numero2)
+
+//var numero1 = Number(prompt("qual a primeira nota do " + nome ))
+//var numero2 = Number(prompt("ok. Primeira nota 10. Qual a segunda nota?"))
+//alert("a medida final será " + (numero1 + numero2) / 2)
+
+
+
+
+//if (nota >= 60 ){
+//alert ("nota total: " + nota + " . APROVADO")
+//}
+//else {
+
+//alert ("nota total: " + nota + " . REPROVADO")
+//}
+
+
+
+
+
+//var imc = (peso / (altura * altura)).toFixed(2)
+
+//alert(imc)
+
+//if (imc >= 18.9){
+//alert ("muito abaixo do peso")
+//}
+//else if (imc >= 17 && imc <= 18.4){
+//  alert("abaixo do peso")
+//}
+//else if (imc >= 17 && imc <= 18.4){
+//   alert("abaixo do peso")
+//}
+//else if (imc >= 17 && imc <= 18.4){
+//    alert("abaixo do peso")
+//}
+//else if (imc >= 17 && imc <= 18.4){
+//    alert("abaixo do peso")
+//}
+//else if (imc >= 17 && imc <= 18.4){
+//  alert("abaixo do peso")
+//}
+//else if (imc >= 17 && imc <= 18.4){
+//   alert("abaixo do peso")
+//}
+//else if (imc >= 17 && imc <= 18.4){
+// alert("abaixo do peso")
+//}
+//else if (imc >= 17 && imc <= 18.4){
+//   alert("abaixo do peso")
+//}
+//else if (imc >= 17 && imc <= 18.4){
+//    alert("abaixo do peso")
+//}
+
+
+
+
+
+//var listaDeNomes = ["Maylon", "Arthur", "Ryan", "Raphael", "Maria", "Gabriel", "Macoly", "Dalton", "Patrick" ]
+
+//alert ( ListaDeCursos[0] )
+//alert ( ListaDeCursos[1] )
+//alert ( ListaDeCursos[2] )
+//alert ( ListaDeCursos[3] )
+//alert ( ListaDeCursos[4] )
+//alert ( ListaDeCursos[5] )
+//alert ( ListaDeCursos[6] )
+//alert ( ListaDeCursos[7] )
+//alert ( ListaDeCursos[8] )
+//alert ( ListaDeCursos[9] )
+//alert ( ListaDeCursos[10] )
+//alert ( ListaDeCursos[11] )
+//alert ( ListaDeCursos[12] )
+
+/*
+ListaDeCursos.forEach(paralelepipedo => alert (paralelepipedo))
+for(var i=0; i<=12; i++)(
+    alert(ListaDeCursos[i])
+)
+*/
+
+//var nota1 = Number(prompt("Qual valor da nota 1? "))
+//var nome2 = Number(prompt("qual o valor da nota 2? "))
+//var nome3 = Number(prompt("qual o valor da nota 3? "))
+//var nome4 = Number(prompt("qual o valor da nota 4? "))
+
+//alert( (nota1 + nome2 + nome3 + nome4) / 4)
+
+//var qtdeNotas = prompt("Quantas notas serão avaliadas?")
+
+//var acumuladorNotas = 0;
+
+
+
+
+//for (var i = 1; i<= qtdeNotas ; i++){
+// var nota = prompt("Digite a nota " + i)
+// acumuladorNotas = acumuladorNotas + Number(nota)
+//}
+
+
+
+//alert("A medida é: " + ( acumuladorNotas / Number(qtdeNotas)))
+
+//ListaDePresença.forEach(nome => alert(nome + " Faz o curso programação"))
+
+//var indiceProcurado = prompt("Digite o indice do aluno que deseja buscar ")
+//alert( ListaDePresença[indiceProcurado])
+
+
+
+//var numero = prompt("Digite um numero")
+
+//for(var i=; i++){
+// alert(i + " x " + numero " = " + (i * numero))
+//}
+
+
+
+
+
+
+
+//var ListaDeJogos = [ "Hollow knigth ", " Red Dead ", " Subnautica ", " Ark Survival Evolved "  ]
+//var novojogo = prompt("Adicione um jogo")
+//ListaDeJogos.push( novojogo )
+
+//alert( ListaDeJogos[4] )
+
+//ListaDeJogos.pop()
+//ListaDeJogos.shift()
+
+///alert( ListaDeJogos[3] )
+
+//ListaDeJogos.splice(2, 1)
+
+//alert( ListaDeJogos )
+
+//ListaDeJogos.reverse()
+
+//alert( ListaDeJogos )
+
+//var indice = ListaDeJogos.indexOf(" Red Dead ")
+
+//alert( indice )
+
+//ListaDeJogos.forEach(jogo => alert("Estou jogando " + jogo))
+
+
+//ar ListaDeJogos = [ "Hollow knigth ", " Red Dead ", " Subnautica ", " Ark Survival Evolved "  ]
+
+//var qtdeJogosNovos = prompt("Quantos jogos vc quer adicionar?")
+
+//for(var i=0; i<qtdeJogosNovos; i++){
+// var jogo = prompt("Digite o nome do jogo")
+//}
+
+//alert(ListaDeJogos)
+
+
+
+
+/*
+var i = 0;
+while( 1 < 10 ){
+alert(i)
+i++
+}
+*/
+
+
+
+/*
+while (operação != 3) {
+  var operação = prompt("1- Saldo\n2- Trasferencia\n3- Sair do sistema")
+}
+  */
+
+
+/*
+var curso = {
+  nome: " logica de programação",
+  alunos: listaDeNomes,
+  cargaHoraria: 60,
+  materia: " javascript",
+  dataInicio: "27/08/2024",
+  datafinal: "29/09/2024",
+}
+
+alert( curso.nome + " é um curso em " + curso.cargaHoraria + "h de carga horaria")
+*/
+
+
+
+/*
+function rotinaDiaria() {
+  alert("acordar")
+  alert("arrumar a cama")
+  alert("ir pra escola")
+  alert("voltar pra casa")
+  alert("mimir")
+}
+  
+rotinaDiaria()
+*/
+
+
+
+
+/*
+function somarNumeros(num1, num2){
+  var num1 = Number(prompt("digite o primeiro"
+  var num2 = Number()
+  var resultado = num1 + num2
+  alert(resultado)
+}
+
+somarNumeros(10, 50)
+*/
+
+
+
+/*
+function somarNumeros(nu1, num2, num3){
+  var num1 = Number(prompt("digite o primeiro numero"))
+  var num2 = Number(prompt("digite o segundo numero"))
+  var num3 = Number(prompt("digite o terceiro numero"))
+  var resultado = num1 * num2 * num3
+  alert(resultado)
+}
+
+somarNumeros()
+
+
+
+function classificador( parâmetro ){
+  num1 = Number(prompt("digite um numero"))
+
+}
+*/
+
+
+
+/*
+var cpf = "133.424.736-76"
+alert( cpfFormatado )
+
+var cpf = "133.424.736-76"
+*/
+
+
+
+var operacao = prompt("1- Treecko\n2- Grovyle\n3- sceptile\n4- Torchic\n5- Combusken\n6- Blaziken\n7- Mudkip\n8- Marshtomp\n9- Swampert\n10- Sair ")
+
+var allStartersHoenn = [
+  { nome: "Treecko", peso: 5, altura: 0.51, hab: "Overgrow ", tipo: "Grass" }, 
+  { nome: "Grovyle", peso: 21.6, altura: 0.89, hab: "overgrow ", tipo: "Grass" }, 
+  { nome: "Sceptile ", peso: 52.2, altura: 1.70, hab: "Overgrow ", tipo: "Grass" }, 
+  { nome: "Torchic ", peso: 2.5, altura: 0.41, hab: "Blaze ", tipo: "Fire" },
+  { nome: "Combusken ", peso: 19.5, altura: 0.89, hab: "blaze ", tipo: "Fire/Fighting" },
+  { nome: "Blazinken ", peso: 52.0, altura: 1.91, hab: "blaze ", tipo: "Fire/Fighting" }, 
+  { nome: "Mudkip ", peso: 7.6, altura: 0.41, hab: "Torrent ", tipo: "Water" }, 
+  { nome: "Marshtomp ", peso: 28.0, altura: 0.71, hab: "Torrent ", tipo: "Water/Ground" }, 
+  { nome: "Swampert ", peso: 81.9, altura: 1.50, hab: "Torrent ", tipo: "Water/Ground" }]
+
+/*
+  var Starters = "";
+allStartersHoenn.forEach(pokemon => Starters = Starters + allStartersHoenn.nome + "[" + + "]" + "\n ")
+
+if (operacao == 1) {
+
+}
+*/
+
+alert( allStartersHoenn[operacao - 1].nome + " pesa " + allStartersHoenn[operacao - 1].peso + " kg\n " + " mede " + allStartersHoenn[operacao - 1].altura + " \nsua habilidade é: " + allStartersHoenn[operacao - 1].hab + " \ntipo: " + allStartersHoenn[operacao - 1].tipo
+     
+)
+
+
+
+
+
+
+
+
+
+
+
+//alert(allStartersHoenn[0].nome)
+
+/*
+allStartersHoenn.forEach(starters => alert (starters))
+for(var i=0; i<=12; i++)(
+    alert(allStartersHoenn[i])
+)
+*/
+
+
+/*
+var starters = {
+  starter: "Treecko",
+  peso: "5.0 kg",
+  altura: "0.51 m",
+  estagio2: allStartersHoenn[1],
+}
+*/
+
+
